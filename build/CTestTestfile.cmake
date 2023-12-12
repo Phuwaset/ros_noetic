72edbea2d68bd73960cf0cbbb89c492ca4f8b42d
PhuwasetNeotic <phuwaset204@gmail.com>
2023-12-12 14:04:33 +0700

    backup
    add commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/ros_noetic/src
+# Build directory: /home/ubuntu/catkin_ws/ros_noetic/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_pkg")
