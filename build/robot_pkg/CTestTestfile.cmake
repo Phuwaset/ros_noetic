dbe99132ed5fa38d89610a0dff31d2f712e49027
PhuwasetNeotic <phuwaset204@gmail.com>
2023-12-14 19:05:28 +0700

    update
    update

---
+++ build/robot_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/ros_noetic/src/robot_pkg
+# Build directory: /home/ubuntu/catkin_ws/ros_noetic/build/robot_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
